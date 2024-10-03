c2c188fed4731bf458e8f1700dd906320fa0872c
Ulfat Zakirli <ulfetzakirli@gmail.com>
2024-10-03 21:04:47 +0400

    feat: function

--- classwork/08.ArrayMethods/script.js
+++ classwork/08.ArrayMethods/script.js
@@ -1,10 +1,8 @@
 // map,filter,slice,findIndex,sort,reverse,push,pop,unshift,shift
 
-
-
-// push - to add end of the array 
+// push - to add end of the array
 // pop - remove last item from array
-const obj = {}
+const obj = {};
 // users.push('Ali', 'Fidan', 7, true, undefined, obj, false, null, NaN)
 // const deletedItem = users.pop()
 // users.pop()
@@ -32,7 +30,6 @@ const obj = {}
 // console.log('users', users)
 // console.log('splicedData', splicedData)
 
-
 // slice - String, Array
 // splice - Array
 // console.log(users);
@@ -46,7 +43,6 @@ const obj = {}
 // console.log('result', result)
 // users.slice(2).reverse()
 // console.log('reversed', users);
-
 
 // splice,reverse,sort,(pop,push,shift,unshift)
 // const users = ['Sabir', 'Fatime', 'Gunel', 'Cavid', 'Xanlar', 9, 10, 11]
@@ -77,7 +73,6 @@ const obj = {}
 
 // ES5 - ES12(ECMAScript5)
 
-
 // const result = numbers.filter((item, index) => item > 2000000000000000)
 // const result2 = numbers.find((item, index) => item > 4000000000000000)
 
@@ -88,8 +83,6 @@ const obj = {}
 // users.sort((sabir, ulfat) => ulfat.localeCompare(sabir))   //(a - b) //asc
 // // users.sort((a, b) => b.localeCompare(a))   //(b - a) //desc
 // console.log('users', users)
-
-
 
 // const numbers = [9, 5, 3, 11, 12, 33, 22, 9, 11, 10, 0, 8]
 
@@ -108,8 +101,6 @@ const obj = {}
 // 11.sort asc
 // 12.sort desc
 
-
-
 // letters.sort((a, b) => {
 //     if (typeof a === 'number' && typeof b === 'number') {
 //         return a - b
@@ -127,9 +118,7 @@ const obj = {}
 //     console.log('success')
 // }
 
-
 // 11 > 8 ? console.log('success') : console.log('fail');
-
 
 // filter,reverse,splice,map,slice,sort,shift,unshift,pop,push,find,includes,findIndex,concat,at
 // reduce,flat,some,every
@@ -152,7 +141,6 @@ const obj = {}
 // console.log('result', result)
 // console.log('result2', result2)
 
-
 // console.log(Math.min(...nums) * Math.max(...nums));
 
 // nums.sort((a, b) => a - b)
@@ -164,8 +152,6 @@ const obj = {}
 
 // console.log(users.flat(Infinity))
 
-
-
 // let sum = 0
 // numbers.map((item) => sum += item)
 // console.log('sum', sum)
@@ -174,17 +160,71 @@ const obj = {}
 // console.log('sum2', sum2)
 
 // Class Task
-const numbers = [9, 5, 3, 11, 12, 33, 22, 9, 11, 10, 0, 8]
-const list = ['6', '3', true, 'a', undefined, false, 10, 11, 5]
-const list2 = ['a', ['f'], 7, ['d', [false]]]
-
+const numbers = [9, 5, 3, 11, 12, 33, 22, 9, 11, 10, 0, 8];
+const list = ["6", "3", true, "a", undefined, false, 10, 11, 5];
+const list2 = ["a", ["f"], 7, ["d", [false]]];
 
 // 1.check 'numbers' if any item is greater than 5 (some)
 // 2.check 'numbers' if all item is greater than 2 (every)
 // 3.flat 'list2' deeps(2) (flat)
 // 4.flat 'list2' deeps(Infinity) (flat)
 // 5.sum all 'numbers' (reduce)
-// 6.sum only type number in 'list' (reduce)
+// 6.sum only 'type number' in 'list' (reduce)
 // 7.check 'list' array that all item is true value (not falsy value) (every)
-// 8.sort only numbers in 'list'
+// 8.sort only numbers in 'list'(sort)
+
+// Ternary Operator (? :)
+// 7 > 5 ? console.log('yes') : console.log('no');
+
+const test = ["a", "b", "c", 6, true, false, undefined, null, "d"];
+const result = test.reduce(
+    (acc, item) => (typeof item === "string" ? acc + item : acc),
+    ""
+);
+console.log(result);
+// let x = 9;
+// if (x > 10) {
+//     'yes'
+// } else {
+//     'no'
+// }
+
+// x > 10 ? 'yes' : 'no';
+
+// reduce,every,some,flat,unshift,shift,pop,push,slice,splice,contact,reverse,includes, find, findIndex, filter, map, at, sort
+
+// console.log(arr.every((item) => Boolean(item)));
+// console.log(arr.reduce((acc, item) => acc + item, 0));
+
+// console.log(Number(null));
+// console.log(Number(undefined));
+// const test2 = [1, [2, [3, [4]]]]
+// console.log('test2', test2.flat(Infinity))
+// arr.splice(2, 3)
+// arr.slice(2, 3)
+// false, 0, undefined, '', NaN, null,"",``
+
+// splice,reverse,sort
+
+// console.log('arr', arr)
+// const sliced = arr.slice(0, 3)
+// console.log('arr sliced', sliced)
+
+
+// forEach
+// const arr = [1, 4, 2, 7, undefined, 'Fatime', 'Sabir']
+// const result2 = arr.findIndex((item) => typeof item === 'number' && item > 3)
+// console.log(result2);
+
+// console.log(arr.map(item => item));
+// console.log(arr.map(item => {
+//     return item
+// }));
+
+// console.log(arr.forEach((item) => {
+//     console.log(item);
+// }));
+
+
+
 
