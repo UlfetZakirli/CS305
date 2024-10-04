afdea43b94de2eeabf44edcadddd89d180de640a
Ulfat Zakirli <ulfetzakirli@gmail.com>
2024-10-04 21:07:04 +0400

    feat: HTML Form

--- classwork/10.HTML/script.js
+++ classwork/10.HTML/script.js
@@ -7,43 +7,42 @@ const password = document.getElementById('password')
 const email = document.getElementById('email')
 const user_form = document.getElementById('user_form')
 
-// firstName.addEventListener('input', (e) => {
-//     console.log(e.target.value);
-// })
+firstName.addEventListener('input', (e) => {
+    console.log(e.target.value);
+})
 
-// password.addEventListener('input', (e) => {
-//     console.log(e.target.value);
-// })
+password.addEventListener('input', (e) => {
+    console.log(e.target.value);
+})
 
-// email.addEventListener('input', (e) => {
-//     console.log(e.target.value);
-// })
+email.addEventListener('input', (e) => {
+    console.log(e.target.value);
+})
 
-// const DB_URL = 'https://codecyber/cs305'
+const users = []
 
+user_form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const user = {
+        firstName: '',
+        email: '',
+        password: '',
+    }
+    user.firstName = firstName.value
+    user.email = email.value
+    user.password = password.value
+    users.push(user)
+    console.log('users', users)
+})
 
-// const users = []
-
-// user_form.addEventListener('submit', (e) => {
-//     e.preventDefault()
-//     const user = {
-//         firstName: '',
-//         email: '',
-//         password: '',
-//     }
-//     user.firstName = firstName.value
-//     user.email = email.value
-//     user.password = password.value
-//     users.push(user)
-//     console.log('users', users)
-//     fetch(DB_URL, {
-//         method: 'POST'
-//     }, users)
-// })
 
 // POST - send data
 // DELETE - delete data
 // PUT / PATCH - update data
+
+
+// Class Task
+// inputs- firstName,lastName,email,password,confirmPassword
 
 
 
